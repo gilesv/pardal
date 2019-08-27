7ad77a9a59b122b58d2a936e55c835867b88d8fb
gilesv <gilesv@protonmail.com>
2019-08-27 13:03:31 -0300

    Extra configuration for deploy at gh-pages

---
+++ docs/precache-manifest.a3ddc28594354365f49ef898f2774279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3854967f3c476b0d18821911aa38938e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23915ac0fc5bc79d120d",
+    "url": "/static/css/2.70d6cb51.chunk.css"
+  },
+  {
+    "revision": "2306b1b22ecaad09ffb5",
+    "url": "/static/css/main.4d847b88.chunk.css"
+  },
+  {
+    "revision": "23915ac0fc5bc79d120d",
+    "url": "/static/js/2.8c29c827.chunk.js"
+  },
+  {
+    "revision": "2306b1b22ecaad09ffb5",
+    "url": "/static/js/main.23c272ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
